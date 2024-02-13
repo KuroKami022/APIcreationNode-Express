f96bf3201c47f327495d9da78a685656b35c02f5
KuroKami022 <luisru693@gmail.com>
2024-02-12 19:57:22 -0500

    config finished , dev server launched

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('MyApp');
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) =>{
+  res.send("Hola mi server en Express");
+});
+
+app.get("/nueva-ruta", (req, res) =>{
+  res.send("Hola new route");
+});
+
+app.get("/products", (req, res) =>{
+  res.json({
+    name: 'Product 1',
+    price: 1000
+  })
+});
+
+app.listen(port, () =>{
+  console.log("My port: " + port);
+});
