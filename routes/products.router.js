ae6ed1e46b36af9a32475ff7db6e245d39f4a2a9
KuroKami022 <luisru693@gmail.com>
2024-02-19 17:35:56 -0500

    ready to deploy

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,49 +1,60 @@
 const express = require('express');
-
 const ProductsService = require('./../services/product.service');
-
+const validatorHandler = require('./../middlewares/validator.handler');
+const {
+  createProductSchema,
+  updateProductSchema,
+  getProductSchema,
+} = require('./../schemas/product.schema');
 const router = express.Router();
 const service = new ProductsService();
-
-router.get('/', (req, res) => {
-  const products = service.find();
+router.get('/', async (req, res) => {
+  const products = await service.find();
   res.json(products);
 });
-
 router.get('/filter', (req, res) => {
   res.send('Yo soy un filter');
 });
-
-router.get('/:id', (req, res) => {
+router.get(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const product = await service.findOne(id);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+router.post(
+  '/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  }
+);
+router.patch(
+  '/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const product = await service.update(id, body);
+      res.json(product);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+router.delete('/:id', async (req, res) => {
   const { id } = req.params;
-  const product = service.findOne(id);
-  res.json(product);
+  const rta = await service.delete(id);
+  res.json(rta);
 });
-
-router.post('/', (req, res) => {
-  const body = req.body;
-  res.status(201).json({
-    message: 'created',
-    data: body
-  });
-});
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id,
-  });
-});
-
 module.exports = router;
