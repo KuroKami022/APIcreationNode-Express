27f9b652f87116f28975eeb301e89579c896f3e4
KuroKami022 <luisru693@gmail.com>
2024-02-19 13:17:45 -0500

    big load / routes / services / middlewares

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require("express");
+const express = require('express');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) =>{
-  res.send("Hola mi server en Express");
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hola mi server en express');
 });
 
-app.get("/nueva-ruta", (req, res) =>{
-  res.send("Hola new route");
+app.get('/nueva-ruta', (req, res) => {
+  res.send('Hola, soy una nueva ruta');
 });
 
-app.get("/products", (req, res) =>{
-  res.json({
-    name: 'Product 1',
-    price: 1000
-  })
-});
+routerApi(app);
 
-app.listen(port, () =>{
-  console.log("My port: " + port);
+
+app.listen(port, () => {
+  console.log('Mi port' +  port);
 });
